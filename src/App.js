5b5f1adda165faefa99bf591b5f19b248a7d6d63
Lomoee <favoursunday600@gmail.com>
2024-02-08 11:59:11 +0100

    added-a-disabled-color

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-
 function App() {
-  const letters = Array.from({ length: 5 }, (_, index) => String.fromCharCode(65 + index));
+  const letters = Array.from({ length: 26 }, (_, index) => String.fromCharCode(65 + index));
+  const [selectedLetter, setSelectedLetter] = useState(null);
+
+  const handleLetterClick = (letter) => {
+    setSelectedLetter(letter);
+  };
+
   return (
     <div className="table-container">
-    <table>
-      <thead>
-        <tr>
-          <th></th>
-          <th>Name</th>
-          <th>Place</th>
-          <th>Animal</th>
-          <th>Thing</th>
-        </tr>
-      </thead>
-      <tbody>
-      {letters.map(letter => (
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Place</th>
+            <th>Animal</th>
+            <th>Thing</th>
+          </tr>
+        </thead>
+        <tbody>
+          {letters.map(letter => (
             <tr key={letter}>
-              <td>{letter}</td>
-              <td contentEditable="true"></td>
-              <td contentEditable="true"></td>
-              <td contentEditable="true"></td>
-              <td contentEditable="true"></td>
+              <td onClick={() => handleLetterClick(letter)}>{letter}</td>
+              <td className={selectedLetter !== letter ? 'disabled' : ''} contentEditable={selectedLetter === letter}></td>
+              <td className={selectedLetter !== letter ? 'disabled' : ''} contentEditable={selectedLetter === letter}></td>
+              <td className={selectedLetter !== letter ? 'disabled' : ''} contentEditable={selectedLetter === letter}></td>
+              <td className={selectedLetter !== letter ? 'disabled' : ''} contentEditable={selectedLetter === letter}></td>
             </tr>
           ))}
-        {/* Add more rows as needed */}
-      </tbody>
-    </table>
-  </div>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
