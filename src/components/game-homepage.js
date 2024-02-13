f71980d0695e7bfdcc3dd856f2974368439f05fb
Lomoee <favoursunday600@gmail.com>
2024-02-13 15:04:27 +0100

    adding section to the how to play

--- src/components/game-homepage.js
+++ src/components/game-homepage.js
@@ -162,6 +162,13 @@ function App() {
                               <p>The game is simple. The game creator sets the number of rounds and chooses the word categories.
 The game is player over a couple of rounds. At the beginning of every round, players get a new alphabet. Your job is to think of words that begin with that letter and fit the categories in the game.
 Each game round is timed at 60 seconds, but the game alsos end as soon as the first person submits their response. Think fast!</p>
+
+                              <h1>Creating A Game and Game Setup</h1>
+
+                              <p>Head on over to the "Create New Game" section on the previous page to create a new game. You will be able to specify the number of rounds you would like to play as well as any additional categories you would want to include besides, "Name", "Place", "Animal" and "Thing".
+You can also define "scoring mechanics". Select "Score Yourself" if you want people to score themselves, or select "Score Each Other" to score your friends.
+Once you have created the game, you will get a "Game Code". Create the room and send the Game Code to your friends who will be able to use it join the room. Once your friends have joined, the creator will be able to start the game!</p>
+
                               <button onClick={joinhandleContinueClick} className='mt-5 bg-black text-white w-52 h-8 rounded'>Continue</button>
                             </div>
                           </div>
